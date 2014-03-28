6e857cf4c393407e27d7f47c850f370fba7595b8
parkito <core.safecrlf>
2014-03-29 00:55:45 +0400

    2

--- 7.cpp
+++ 7.cpp
@@ -1,13 +1,18 @@
 #include <iostream>
+#include <algorithm>
 #include <stdio.h>
 #include <conio.h>
 #include <string>
 #include <fstream>
 int provFile(char);
+int Find (int b[100], int, int, int );
+void quick(int b[100] , int , int );
 using namespace std;
 int main()
-{   fstream in;
+{  
+	fstream in;
     ofstream out;
+	int b[100];
 	char a;
 	int glas=0,soglas=0;
 	setlocale(LC_ALL,"Rus");
@@ -21,19 +26,28 @@ int main()
 	        cin.get();
             return 0;
          }
+		  int j=0;
      while(1) 
-	    { 
-             in>>a;
+	    {    
+             in.get(a);
+			 if (in.eof()) break;
+			 if(a!=' ')
+			 { b[j]=provFile(a);j++;}
 			 
-cin.get(a);
-
-             if (in.eof()) break;
-             if(provFile(a)==0)glas++;
-			 else if(provFile(a)==1)soglas++;
+			 else
+				 {	quick(b,0,j-1);
+					for(int i=0;i<j;i++)
+		        	glas+=Find(b,i,j,0);
+			        for(int i=0;i<j;i++)
+			        soglas+=Find(b,i,j,1);
+					j=0;
+			     }
+            
 			 out<<a;
-        }
+			 
+	    }
+	// sort(b, b + j);
 	 cout<<"Гласные: "<<glas<<" Согласные: "<<soglas<<endl;
-	 //cout<<
 	 in.close();
      out.close();
 	 cin.get();
@@ -42,7 +56,7 @@ cin.get(a);
 }
 int provFile(char a)
 { int count;
-	if (a=='A'||a=='a') count=1;
+	if (a=='A'||a=='a') count=0;
 	else if (a=='B'||a=='b') count=0;
 	else if (a=='C'||a=='c') count=1;
 	else if (a=='D'||a=='d') count=1;
@@ -72,3 +86,63 @@ int provFile(char a)
 	return count;
 	
 }
+int Find(int b[100], int k, int l, int x)
+{   int c=0;
+    int first = k;
+    int last = l;
+   /* if (l == 0) 
+	   {
+		cout<<"Слово слишком коротко ";
+       }
+	else if (b[0] > x) 
+	   {
+       cout<<"Не найдено";
+       } 
+	*/
+ 
+    while (first < last)
+	    {
+       
+          int mid = first + (last - first) / 2;
+          if (x <= b[mid])
+            last = mid;
+          else
+            first = mid + 1;
+        }
+ 
+    
+    if (b[last] == x) 
+        c++;
+    
+	return c;
+}
+void quick(int a[100] , int l, int r)
+	     {
+		   int x = a[l + (r - l) / 2];
+		   //запись эквивалентна (l+r)/2,
+		   //но не вызввает переполнения на больших данных
+		   int i = l;
+		   int j = r;
+		   //код в while обычно выносят в процедуру particle
+		   while(i <= j)
+		      { 
+				  
+		          while(a[i] < x) {i++;}
+		          while(a[j] > x) {j--;}
+
+		          if(i <= j)
+		             {  
+						 swap(a[i], a[j]);
+		                 if(a[i]<a[j])
+		                 i++;
+		                 j--;
+		               }
+		       }
+		   if (i<r)
+			   quick(a,i, r);
+
+		   if (l<j)
+			   quick(a,l,j);
+
+		  
+	   }
